48fe662a64c124792d595ef2fc83be603dbbe2f3
Michael Bianco <beyawnko290@gmail.com>
2025-06-13 22:13:47 -0500

    Add cppcheck config and update script

--- tools/run_cppcheck.sh
+++ tools/run_cppcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 QT_INCLUDE_DIR=$(pkg-config --variable=includedir Qt5Core)
-cppcheck --enable=all --library=qt \
+cppcheck --enable=all --library=qt --template=gcc \
+         @tools/cppcheck.cfg \
          -I "$QT_INCLUDE_DIR" -I "$QT_INCLUDE_DIR/QtWidgets" \
          . 2> cppcheck.log
